9f363680b3c63862eee0e927684326797f3cee53
Michael Maguire <github@michaelmaguire.ca>
2025-01-12 16:32:51 +0100

    Compiling and advancing guard, not rotating yet

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,141 @@
-use std::cmp::Ordering;
 use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::env;
-use std::collections::HashSet;
 
 #[derive(Default)]
 struct MapMatrix {
     matrix: Vec<Vec<char>>,
+    
 }
+
+#[derive(Clone, Copy, Debug, Eq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+impl PartialEq for Direction {
+    fn eq(&self, other: &Self) -> bool {
+        *self as u8 == *other as u8
+    }
+}
+impl Direction {
+    fn from_char(c: char) -> Option<Self> {
+        match c {
+            '^' => Some(Direction::North),
+            '>' => Some(Direction::East),
+            'v' => Some(Direction::South),
+            '<' => Some(Direction::West),
+            _ => None,
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Direction::North => '^',
+            Direction::East => '>',
+            Direction::South => 'v',
+            Direction::West => '<',
+        }
+    }
+}
+
+impl Direction {
+    fn rotate(&self) -> Direction {
+        match self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        }
+    }
+}
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_char())
+    }
+}
+
+#[derive(Copy, Eq, Clone)]
+struct Guard {
+    x: usize,
+    y: usize,
+    orientation: Direction,
+}
+impl fmt::Debug for Guard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "Guard {{ x: {}, y: {}, orientation: '{}' }}", self.x, self.y, self.orientation)
+    }
+}
+
+impl PartialEq for Guard {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y && self.orientation == other.orientation
+    }
+}
+
+
+impl Guard {
+    fn advance_coords(&self) -> (usize, usize){
+        let (new_x, new_y) = match self.orientation {
+            Direction::South => (self.x, self.y + 1),
+            Direction::North => (self.x, self.y.wrapping_sub(1)),
+            Direction::West => (self.x.wrapping_sub(1), self.y),
+            Direction::East => (self.x + 1, self.y),
+        };
+        (new_x, new_y)
+    }
+
+
+    // As termination condition, returns same position and orientation if we cannot move or we went off the map.
+    fn move_guard(self, map_matrix: &MapMatrix) -> Self {
+        let (new_x, new_y) = self.advance_coords();
+
+        if map_matrix.in_bounds(new_x, new_y) {
+            // new coordinates are within the map boundaries, see if we can move there.
+            if ! map_matrix.is_obstacle(new_x, new_y) {
+                // Return new position and orientation.
+                Guard { x: new_x, y: new_y, orientation: self.orientation }
+            } else {
+                // We are in map but would ry into an obstacle, so changing orientation of original and advancing works.
+                let mut tentative_new_guard = self.clone();
+                for _ in 0..3 {
+                    tentative_new_guard.orientation.rotate();
+                    let ( new_x, new_y ) = tentative_new_guard.advance_coords();
+                    tentative_new_guard.x = new_x;
+                    tentative_new_guard.y = new_y;
+                    if map_matrix.in_bounds(tentative_new_guard.x, tentative_new_guard.y) && ! map_matrix.is_obstacle(tentative_new_guard.x, tentative_new_guard.y)  {
+                        return tentative_new_guard;
+                    }
+                }
+                // We tried all orientations and none worked, so return Guard unchanged and indication to stop.
+                self
+            }
+        } else {
+            // We went off the map, so return Guard unchanged and indication to stop.
+            self
+        }
+    }
+}
+
+
 
 impl fmt::Debug for MapMatrix {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         let height = self.matrix.len();
         let width = self.matrix[0].len();
-        for i in 0..height {
-            for j in 0..width {
-                let val = self.matrix[i][j];
-                write!(f, "{val} ");
+        for y in 0..height {
+            for x in 0..width {
+                let val = self.matrix[y][x];
+                if let Err(e) = write!(f, "{val} ") {
+                    panic!("Writing error: {e}");
+                }
             }
-            writeln!(f);
+            if let Err(e) = writeln!(f) {
+                panic!("Writing error: {e}");
+            }
         }
         Ok(())
     }
@@ -28,26 +143,61 @@ impl fmt::Debug for MapMatrix {
 
 trait LoadInput {
     fn push(&mut self, the_vec_to_add: Vec<char> );
+    fn find_guard(&self) -> Result<Guard, &'static str>;
 }
 
 trait Obstacle {
     fn set_char(&mut self, x: usize, y: usize, the_char: char);
     fn get_char(&self, x: usize, y: usize) -> char;
+    fn height(&self) -> usize;
+    fn width(&self) -> usize;
+    fn in_bounds(&self, x: usize, y: usize) -> bool;
+    fn is_obstacle(&self, x: usize, y: usize ) -> bool;
     }
 
 impl Obstacle for MapMatrix {
     fn set_char(&mut self, x: usize, y: usize, the_char: char) {
-        self.matrix[x][y] = the_char;
+        self.matrix[y][x] = the_char;
     }
     fn get_char(&self, x: usize, y: usize) -> char {
-        return self.matrix[x][y];
+        return self.matrix[y][x];
     }
+    fn height(&self) -> usize {
+        return self.matrix.len();
+    }
+    fn width(&self) -> usize {
+        if self.matrix.len() > 0 {
+            // Assumes uniform.
+            return self.matrix[0].len();
+        }
+        return 0;
+    }
+    fn in_bounds(&self, x: usize, y: usize) -> bool {
+        x < self.width() && y < self.height()
+    }
+    fn is_obstacle(&self, x: usize, y: usize ) -> bool {
+        self.get_char(x, y) == '#'
+    }
+}
 
+fn is_guard(the_char: char) -> Option<Direction> {
+    Direction::from_char(the_char)
 }
 
 impl LoadInput for MapMatrix {
     fn push(&mut self, the_vec_to_add: Vec<char> ) {
         self.matrix.push(the_vec_to_add);
+    }
+    fn find_guard(&self) -> Result<Guard, &'static str> {
+        for x in 0.. self.width() {
+            for y in 0..self.height() {
+                let the_char = self.get_char(x, y);
+                if let Some(the_orientation) = is_guard( the_char ) {
+                    return Ok(Guard { x: x, y : y, orientation: the_orientation})
+                }
+            }
+        }
+        return Err("No guard found");
     }
 }
 
@@ -82,6 +232,31 @@ fn main() {
         }
 
         println!("\nmap_matrix: \n{:?}", map_matrix);
+
+        let mut the_guard = map_matrix.find_guard().unwrap();
+        println!("Guard: {:?}", the_guard);
+        
+        loop {
+            println!("Analyzing guard: {:?}", the_guard);
+            // Add your analysis logic here
+            let new_guard = the_guard.move_guard(&map_matrix);
+            if new_guard != the_guard {
+                println!("Guard moved from {:?} to {:?}", the_guard, new_guard);
+                // Mark where we visited.
+                map_matrix.set_char(new_guard.x, new_guard.y, new_guard.orientation.to_char());
+            } else {
+                println!("Guard did not move");
+                break;
+            }
+
+            the_guard = new_guard;
+
+            println!("\nmap_matrix: \n{:?}", map_matrix);
+
+        }
+
+        println!("\nFINAL map_matrix: \n{:?}", map_matrix);
+
 
     }
 }
